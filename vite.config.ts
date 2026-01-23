cf333dd4bc9a6068f971a46c452e9bf559c2d4df
JStanlee <72189913+JStanlee@users.noreply.github.com>
2026-01-23 22:13:11 +0100

    Podobno ok

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    assetsDir: 'assets',
   },
-  server: {
-    port: 3000
+  define: {
+    'process.env': {}
   }
 });
